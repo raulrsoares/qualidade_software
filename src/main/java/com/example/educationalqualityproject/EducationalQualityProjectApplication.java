2075c48db1649bc14135c19bf9ac39b00d4ae215
Raul Soares <83381403+raulrsoares@users.noreply.github.com>
2026-03-24 20:52:44 -0300

    chore: force ci

--- src/main/java/com/example/educationalqualityproject/EducationalQualityProjectApplication.java
+++ src/main/java/com/example/educationalqualityproject/EducationalQualityProjectApplication.java
@@ -9,5 +9,4 @@ public class EducationalQualityProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(EducationalQualityProjectApplication.class, args);
     }
-
 }
